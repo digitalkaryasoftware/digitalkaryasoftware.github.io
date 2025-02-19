b055c119f2ee8c122c247fc723e01bcbcde38c60
digitalkaryasoftware <119415937+digitalkaryasoftware@users.noreply.github.com>
2025-02-19 04:41:43 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+function getUserIP() {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+$user_ip = getUserIP();
+
+echo "Alamat IP Anda: " . htmlspecialchars($user_ip, ENT_QUOTES, 'UTF-8');
+?>
